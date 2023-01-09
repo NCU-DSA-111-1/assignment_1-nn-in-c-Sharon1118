fcf0d416dd72a0f3001956bed26b1517c7433d79
Sharon1118 <sharon98129@gmail.com>
2023-01-10 06:22:45 +0800

    upload
    upload

---
+++ inc/declare.h
@@ -0,0 +1,15 @@
+#ifndef DECLARE_H
+#define DECLARE_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define numInputs 2
+#define numOutputs 1
+#define numTrainingSets 4
+
+void shuffle(int* , size_t );
+
+#endif
